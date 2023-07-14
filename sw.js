f14732eb41c9d2ce8e7a794068db8c973d6d15b1
noobdehierro <joseluisreyesalvarez97@gmail.com>
2023-07-14 14:10:01 -0600

    mas rutas

--- sw.js
+++ sw.js
@@ -3,18 +3,18 @@ self.addEventListener("install", (event) => {
     caches.open("static-v1").then((cache) => {
       cache.addAll([
         "/",
-        "/index.html",
-        "/css/styles.css",
-        "/js/app.js",
-        "/js/jquery.com_jquery-3.7.0.min.js",
-        "/js/jquery.validate.min.js",
+        "index.html",
+        "css/styles.css",
+        "js/app.js",
+        "js/jquery.com_jquery-3.7.0.min.js",
+        "js/jquery.validate.min.js",
         "https://cdn.jsdelivr.net/npm/sweetalert2@11",
-        "/images/fondo.jpg",
-        "/manifest.json",
-        "/images/icon-48x48.png",
-        "/images/icon-96x96.png",
-        "/images/icon-192x192.png",
-        "/images/icon-512x512.png",
+        "images/fondo.jpg",
+        "manifest.json",
+        "images/icon-48x48.png",
+        "images/icon-96x96.png",
+        "images/icon-192x192.png",
+        "images/icon-512x512.png",
       ]);
     })
   );
